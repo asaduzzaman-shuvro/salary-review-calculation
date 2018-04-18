4ceb1b45e9d6863fd9e2ab7f8c5103fd9c904b9a
atiqul alam <atiqul@smartitengineering.com>
2018-04-18 14:29:22 +0600

    Implement salary review for project.

--- src/main/java/com/cefalo/school/App.java
+++ src/main/java/com/cefalo/school/App.java
@@ -3,8 +3,11 @@ package com.cefalo.school;
 import com.cefalo.school.calculator.*;
 import com.cefalo.school.data.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class App {
 
@@ -32,6 +35,9 @@ public class App {
 
         Team team = getDummyTeam();
         System.out.println("Team Review: " + TeamReviewCalculator.calculate(team));
+
+        Project project = getDummyProject();
+        System.out.println("Project Review: " + ProjectReviewCalculator.calculate(project));
 
 
 
@@ -70,7 +76,40 @@ public class App {
 
     }
 
-    private Project getDummyProject(){
-        return null;
+    public static Project getDummyProject(){
+        Impact impact = new Impact(0.7,0.8,0.8,0.9,0.8);
+        // team 1
+        Entity entityTeam1 = new Entity("GB", "11");
+        Score devScore11 = new Score(8, 3, 2, 7, 10);
+        Employee employee11 = new Employee(new Entity("Rocky", "123123"),entityTeam1,Role.Developer, 200000.0,impact, devScore11);
+
+        Score devScore12 = new Score(8, 2, 2, 8, 10);
+        Employee employee12 = new Employee(new Entity("Shuvro", "1223123"),entityTeam1,Role.Developer, 200000.0,impact, devScore12);
+
+        Score devScore13 = new Score(8, 2, 3, 8, 9);
+        Employee employee13 = new Employee(new Entity("Ashif", "12231212123"),entityTeam1,Role.TeamLead, 200000.0,impact, devScore13);
+
+        List<Employee> employeesTeam1 = new ArrayList<>();
+        employeesTeam1.addAll(Arrays.asList(employee11,employee12,employee13));
+        Team team1 = new Team(entityTeam1, employeesTeam1);
+
+        // team 2
+        Entity entityTeam2 = new Entity("NG","12");
+        Score devScore21 = new Score(8, 3, 2, 7, 10);
+        Employee employee21 = new Employee(new Entity("Atiqul", "223123"),entityTeam2,Role.Developer, 200000.0,impact, devScore21);
+
+        Score devScore22 = new Score(8, 2, 2, 8, 10);
+        Employee employee22 = new Employee(new Entity("Assaduzzaman", "2223123"),entityTeam2,Role.Developer, 200000.0,impact, devScore22);
+
+        Score devScore23 = new Score(8, 2, 3, 8, 9);
+        Employee employee23 = new Employee(new Entity("Iqbal", "22231212123"),entityTeam2,Role.TeamLead, 200000.0,impact, devScore23);
+
+        List<Employee> employeesTeam2 = new ArrayList<>();
+        employeesTeam2.addAll(Arrays.asList(employee21,employee22,employee23));
+        Team team2 = new Team(entityTeam2, employeesTeam2);
+
+        Entity projectEntity = new Entity("Global", "111");
+
+        return new Project(projectEntity, Arrays.asList(team1,team2));
     }
 }
--- src/main/java/com/cefalo/school/calculator/ProjectReviewCalculator.java
+++ src/main/java/com/cefalo/school/calculator/ProjectReviewCalculator.java
@@ -1,14 +1,15 @@
 package com.cefalo.school.calculator;
 
 import com.cefalo.school.data.Employee;
+import com.cefalo.school.data.Project;
 import com.cefalo.school.data.Team;
 
 import java.util.List;
 
 public class ProjectReviewCalculator {
-    static Double calculate(List<Team> teams){
+    public static Double calculate(Project project){
         Double review = 0.0;
-        for (Team team: teams){
+        for (Team team: project.getTeams()){
             review += TeamReviewCalculator.calculate(team);
         }
         return review;
